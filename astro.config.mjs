b1bf238469ea480cc3e330842c9c4a870480b614
Avinh Huynh <avinhahuynh@gmail.com>
2023-09-24 17:37:59 -0700

    added tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Enable React to support React JSX components.
+  integrations: [react(), tailwind()]
+});
