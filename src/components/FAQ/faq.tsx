469366f63036bf2119102cb9682d9f51fce9c445
intern008 <wingswithin.intern.08@gmail.com>
2023-10-08 13:15:13 -0700

    FAQ commit
    Basic FAQ skeleton!

---
+++ src/components/FAQ/faq.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './faq.css'
+
+function FAQ() {
+  return (
+    <body>
+        <div className='banner'> Frequently Asked Questions!</div>
+    
+        <div className='container'> 
+        1 Questions come here!!! <br />
+        2 Questions come here!!!<br />
+        3 Questions come here!!!<br />
+        4 Questions come here!!!<br />
+        5 Questions come here!!!<br />
+        6 Questions come here!!!<br />
+        7 Questions come here!!!<br />
+        8 Questions come here!!!<br />
+        9 Questions come here!!!<br />
+        10 Questions come here!!!</div>
+    </body>
+  )
+}
+
+export default FAQ
