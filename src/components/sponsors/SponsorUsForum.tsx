0a89f401a50e8ea9c924d0eb4aeed46d094841b0
AkimT13 <akimtarasov@gmail.com>
2023-11-06 15:16:16 -0800

    new sponsor page layout

--- src/components/sponsors/SponsorUsForum.tsx
+++ src/components/sponsors/SponsorUsForum.tsx
@@ -1,23 +1,38 @@
-export default function SponsorUsForum(){
-    return(
-        <div className= "bg-sfDarkerBlue w-auto h-auto px-4 py-4">
-            <h1 className="text-white text-3xl font-bold pb-4">Reach Us:</h1>
-            <div className="flex flex-col gap-3">
-                <label className="text-white">Name</label>
-                <input type= "text" className=" bg-gray-600 text-gray-300 p-2.5"/>
-                <label className="text-white">Email</label>
-                <input type= "text" className=" bg-gray-600 text-gray-300 p-2.5"/>
-                <label className="text-white">Message</label>
-                <input type= "text" className=" bg-gray-600 text-gray-300 p-2.5 pb-14"/>
-                <button className=" bg-sfGreyBlue rounded-md text-white w-fit px-2 place-self-end "> Submit</button>
-            </div>
+export default function SponsorUsForum() {
+  return (
+    <div className="bg-sfDarkerBlue w-auto h-auto px-4 py-4 md:w-1/2">
+      <h1 className="text-white text-3xl font-bold pb-4">Reach Us:</h1>
+      <form
+        className="flex flex-col gap-3"
+        action="mailto:akimtarasov@gmail.com"
+        method="get"
+        encType="text/plain"
+      >
+        <label className="text-white">Name</label>
+        <input
+          type="text"
+          className=" bg-gray-600 text-gray-300 p-2.5"
+          id="name"
+          name="name"
+        />
+        <label className="text-white">Email</label>
+        <input
+          type="text"
+          className=" bg-gray-600 text-gray-300 p-2.5 "
+          name="email"
+        />
+        <label className="text-white">Message</label>
 
-
-            
-
-        </div>
-    )
-
-    
-
+        <input
+          type="text"
+          className=" bg-gray-600 text-gray-300 p-2.5 pb-14"
+          name="body"
+        />
+        <button className=" bg-sfGreyBlue rounded-md text-white w-fit px-2 place-self-end ">
+          {" "}
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
