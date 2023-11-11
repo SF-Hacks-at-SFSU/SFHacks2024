edad466872b230d523dd4b881581f10133d90ad9
AkimT13 <akimtarasov@gmail.com>
2023-11-10 21:13:33 -0800

    changed to sfhacks email

--- src/components/sponsors/SponsorUsForum.tsx
+++ src/components/sponsors/SponsorUsForum.tsx
@@ -5,7 +5,7 @@ export default function SponsorUsForum() {
       <h1 className="text-white text-3xl font-bold pb-4">Reach Us:</h1>
       <form
         className="flex flex-col gap-3"
-        action="https://formspree.io/f/xvojyyag"
+        action="https://formspree.io/f/xpzgnnda"
         method="POST"
         id="sponsorForm"
       >
