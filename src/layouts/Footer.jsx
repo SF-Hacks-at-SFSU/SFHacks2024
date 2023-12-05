91ceb9f801e8068a7c77b7ea403cc25bac01f374
Avinh Huynh <avinhahuynh@gmail.com>
2023-12-04 17:46:29 -0800

    index page grow better and footer 4 -a pgs

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     const sponsors = [
         {
             name: "Sticker Mule",
-            path: "public/sponsors/sticker-mule-logo-light-bg-stacked.svg",
+            path: "/public/sponsors/sticker-mule-logo-light-bg-stacked.svg",
             link: "https://www.stickermule.com/"
         },
         // {
