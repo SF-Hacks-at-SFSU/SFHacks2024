9404cc48bbd16c4a2bc3f9add15fdc1a0776500d
Avinh Huynh <avinhahuynh@gmail.com>
2023-12-04 17:41:36 -0800

    done with footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,10 +1,27 @@
+import './Footer.css'
+
 export default function Footer() {
 
-    
+    const sponsors = [
+        {
+            name: "Sticker Mule",
+            path: "public/sponsors/sticker-mule-logo-light-bg-stacked.svg",
+            link: "https://www.stickermule.com/"
+        },
+        // {
+        //     name: "Los Pollos",
+        //     path: "public/sponsors/Los_Pollos.webp",
+        //     link: "dinosaur"
+        // }
+    ]
+
+    const sponsorsMap = sponsors.map(x => <a href={x.link}><img className="sponsor--image" src={x.path} /></a>)
 
     return (
-        <div className="Footer">
+        <div className="foot">
             <p>Powered By</p>
+            <div className="sponsors"></div>
+            {sponsorsMap}
         </div>
     )
 }
