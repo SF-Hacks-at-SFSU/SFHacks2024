29b01b71707e74e9c7d3a6ba68c2f23f3694b7ed
AkimT13 <akimtarasov@gmail.com>
2023-12-14 11:59:28 -0800

    opens new tab

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,27 +1,30 @@
-import './Footer.css'
+import "./Footer.css";
 
 export default function Footer() {
+  const sponsors = [
+    {
+      name: "Sticker Mule",
+      path: "/sponsors/sticker-mule-logo-light-bg-stacked.svg",
+      link: "https://www.stickermule.com/",
+    },
+    // {
+    //     name: "Los Pollos",
+    //     path: "public/sponsors/Los_Pollos.webp",
+    //     link: "dinosaur"
+    // }
+  ];
 
-    const sponsors = [
-        {
-            name: "Sticker Mule",
-            path: "/sponsors/sticker-mule-logo-light-bg-stacked.svg",
-            link: "https://www.stickermule.com/"
-        },
-        // {
-        //     name: "Los Pollos",
-        //     path: "public/sponsors/Los_Pollos.webp",
-        //     link: "dinosaur"
-        // }
-    ]
+  const sponsorsMap = sponsors.map((x) => (
+    <a target="_blank" href={x.link}>
+      <img className="sponsor--image" src={x.path} />
+    </a>
+  ));
 
-    const sponsorsMap = sponsors.map(x => <a href={x.link}><img className="sponsor--image" src={x.path} /></a>)
-
-    return (
-        <div className="foot">
-            <p>Sponsored By</p>
-            <div className="sponsors"></div>
-            {sponsorsMap}
-        </div>
-    )
+  return (
+    <div className="foot">
+      <p>Sponsored By</p>
+      <div className="sponsors"></div>
+      {sponsorsMap}
+    </div>
+  );
 }
