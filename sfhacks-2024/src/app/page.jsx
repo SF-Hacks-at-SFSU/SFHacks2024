f53da19fa5e043e835334c42cbe470f6b6f075c9
Avinh Huynh <avinhahuynh@gmail.com>
2024-02-02 21:31:04 -0800

    next im making onscroll animations

--- sfhacks-2024/src/app/page.jsx
+++ sfhacks-2024/src/app/page.jsx
@@ -2,53 +2,51 @@ import Countdown from '../components/Countdown';
 
 export default function Home() {
   return (
-    <section>
-      <div className="h-[calc(100vh)] flex flex-row gap-[10%] justify-center object-cover bg-[url('/brand-assets/background-bridge.png')] bg-cover p-[15%]">
-        <img className="h-[70vh]" src="/brand-assets/purple-logo.png" />
-        <div className="w-[600px] gap-2.5 flex flex-col items-center text-[rgba(245,245,245,0.65)] text-base not-italic font-[550] leading-[normal] text-center">
-          <p>Cohosted by Association of Computer Machinery (ACM) at SFSU</p>
+    <section className="h-[calc(100vh)] flex flex-row gap-[10%] justify-center object-cover bg-[url('/brand-assets/background-bridge.png')] bg-cover p-[15%]">
+      <img className="h-[70vh]" src="/brand-assets/purple-logo.png" />
+      <div className="w-[600px] gap-2.5 flex flex-col items-center text-[rgba(245,245,245,0.65)] text-base not-italic font-[550] leading-[normal] text-center">
+        <p>Cohosted by Association of Computer Machinery (ACM) at SFSU</p>
 
-          <h1 className="text-[#ebebeb] text-center text-[4rem] not-italic leading-[normal] uppercase font-[1000]">SF HACKS 2024</h1>
+        <h1 className="text-[#ebebeb] text-center text-[4rem] not-italic leading-[normal] uppercase font-[1000]">SF HACKS 2024</h1>
 
-          <Countdown />
-          
-          <p>
-            April 5th - 7th 2024
-            <a
-              classNameName="border-b hover:border-sfPink"
-              href="https://www.google.com/maps/place/Student+Life+Events+Center+%2F+Annex+I/@37.7264505,-122.4847354,17z/data=!3m1!4b1!4m6!3m5!1s0x808f7da51ff323d3:0x74d61cd2d66d4dbb!8m2!3d37.7264463!4d-122.4821605!16s%2Fg%2F1hdz113p8?entry=ttu"
-              className="hero--text--subtitle"
-              target="_blank"
-            >
-              @ SF State Annex 1
-            </a>
-          </p>
-
-          <p className="text-white mt-5">
-            Our organization's purpose is to plan and host California's most
-            talented collegiate hackers, designers, and developers, to a 3 day
-            hackathon. Join us next spring to kickoff SF Hacks 2024!
-          </p>
-
-          <div
-            className="inline-block flex-row flex-wrap justify-center gap-5 sm:mb-10"
+        <Countdown />
+        
+        <p>
+          April 5th - 7th 2024
+          <a
+            classNameName="border-b hover:border-sfPink"
+            href="https://www.google.com/maps/place/Student+Life+Events+Center+%2F+Annex+I/@37.7264505,-122.4847354,17z/data=!3m1!4b1!4m6!3m5!1s0x808f7da51ff323d3:0x74d61cd2d66d4dbb!8m2!3d37.7264463!4d-122.4821605!16s%2Fg%2F1hdz113p8?entry=ttu"
+            className="hero--text--subtitle"
+            target="_blank"
           >
-            <a href="https://bit.ly/sfhacks2024-apply" target="_blank"
-              ><button
-                type="button"
-                className="text-sfLightBlue mt-5 p-4 text-lg font-semibold rounded-full bg-sfPink"
-                >Register Now</button
-              >
-            </a>
+            @ SF State Annex 1
+          </a>
+        </p>
 
-            <a href="https://discord.gg/P5PsDR6G7W" target="_blank"
-              ><button
-                type="button"
-                className="bg-sfLightBlue mt-5 p-4 text-lg font-semibold rounded-full text-sfPink"
-                >Join Discord</button
-              >
-            </a>
-          </div>
+        <p className="text-white mt-5">
+          Our organization's purpose is to plan and host California's most
+          talented collegiate hackers, designers, and developers, to a 3 day
+          hackathon. Join us next spring to kickoff SF Hacks 2024!
+        </p>
+
+        <div
+          className="inline-block flex-row flex-wrap justify-center gap-5 sm:mb-10"
+        >
+          <a href="https://bit.ly/sfhacks2024-apply" target="_blank"
+            ><button
+              type="button"
+              className="text-sfLightBlue mt-5 p-4 text-lg font-semibold rounded-full bg-sfPink"
+              >Register Now</button
+            >
+          </a>
+
+          <a href="https://discord.gg/P5PsDR6G7W" target="_blank"
+            ><button
+              type="button"
+              className="bg-sfLightBlue mt-5 p-4 text-lg font-semibold rounded-full text-sfPink"
+              >Join Discord</button
+            >
+          </a>
         </div>
       </div>
     </section>
--- sfhacks-2024/src/components/navbar.jsx
+++ sfhacks-2024/src/components/navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = () => {
 
     return (
         <nav className="flex flex-row items-center justify-between h-24 bg-white">
+
+            {/* Left side of header */}
             <div className="flex flex-row items-center ml-10">
+
+                {/* SF Hacks Logo */}
                 <Link className="" href="/">
                     <Image
                         src="/brand-assets/head-logo.svg"
@@ -15,6 +19,7 @@ const Navbar = () => {
                         />
                 </Link>
 
+                {/* MLH Banner */}
                 <a
                     className="block max-w-24 min-w-14 absolute left-40 top-0 w-1/10 z-10000"
                     href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=red"
@@ -24,24 +29,21 @@ const Navbar = () => {
                     src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
                     alt="Major League Hacking 2024 Hackathon Season"
                 /></a>
+
             </div>
             
-            <div className="flex flex-row mr-10 gap-10">
-                <Link href="/faq/">FAQ</Link>
-                <Link href="/sponsors/">Sponsors</Link>
+            {/* Right side of header */}
+            <div className="flex flex-row mr-10 gap-[5vw]">
+
+                {/* Buttons */}
+                <Link className="transition duration-200 ease-in-out delay-150 border-b-4 hover:border-black border-transparent" href="/faq/">FAQ</Link>
+
+                <Link className="transition duration-200 ease-in-out delay-150 border-b-4 hover:border-black border-transparent" href="/sponsors/">Sponsors</Link>
                 <a
                     href="https://bit.ly/sfhacks2024-apply"
                     target="_blank"
-                    class="register--button">Register
+                    className="transition duration-200 ease-in-out delay-150 border-b-4 hover:border-black border-transparent">Register
                 </a>
-
-
-                {/* .nav--button:hover {
-                    border-color: #132a3e;
-                    border-bottom: 0.25em solid;
-                    cursor: pointer;
-                    transition: 0.2s ease-in;
-                } */}
             </div>
         </nav>
     )
