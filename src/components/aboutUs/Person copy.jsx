414f8e8b5ab5b9352fffdbec76101ed3010c35c2
Avinh Huynh <avinhahuynh@gmail.com>
2024-02-05 15:27:06 -0800

    hard coded images so it runs a little faster

---
+++ src/components/aboutUs/Person copy.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Person = ({ name, position, img }) => {
+  return (
+    <div className="w-40 h-auto  rounded-2xl ">
+      <img
+        src={img}
+        className="aspect-square object-cover rounded-full border-4 border-sfPink object-crop"
+        
+      />
+      <p className="text-white text-center text-4xl p-2">{name}</p>
+      <p className="text-white text-center">{position}</p>
+    </div>
+  );
+};
+
+export default Person;
