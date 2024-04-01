5485f3a3e92c59a783e13fc9e5a5092f8e8e6b61
AkimT13 <akimtarasov@gmail.com>
2024-03-31 21:08:12 -0700

    many changes

--- src/components/Schedule/Event.jsx
+++ src/components/Schedule/Event.jsx
@@ -5,13 +5,13 @@ const Event = ({ start, end, name, description }) => {
   return (
     <div className="md:flex md:flex-row justify-center">
       <div className="flex gap justify-start  bg-sfDarkBlue w-full md:w-[30%]  min-h-16 h-auto border border-sfBloo rounded-2xl">
-        <div className="flex flex-col gap-3 p-3 justify-center ">
+        <div className="flex flex-col gap-3 p-3 justify-center">
           <h1 className="text-md text-sfPink  whitespace-nowrap">{start}</h1>
           <FaArrowDownLong className=" text-sfPink self-center" />
           <h1 className="text-md text-sfPink">{end}</h1>
         </div>
         <div className="flex flex-col justify-center snap-center items-center p-3 w-full">
-          <h1 className="text-center font-bold text-white">{name}</h1>
+          <h1 className="text-center font-bold text-white pb-2">{name}</h1>
           <h1 className="text-center text-gray-200">{description}</h1>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      
+
       colors: {
 				sfGreyBlue : '#838FBA',
 				sfDarkBlue : '#132A3E',
