33132932a77ac7a71ea3ed4aa9fe722a4a911046
Ang197 <angeroderp@gmail.com>
2024-04-07 08:42:55 -0700

    Add congrats message after timer ends

--- src/app/deadline/page.jsx
+++ src/app/deadline/page.jsx
@@ -64,37 +64,47 @@ const page = () => {
     <div className="flex flex-col bg-[bg-[#254d70]] w-full h-full justify-center p-36 bg-[url('/brand-assets/background-bridge.png')] bg-cover gap-5 items-center">
       <div className=" translate-y-20 scale-150">
         <p className="text-center text-white xl:text-6xl md:text-3xl sm:text-sm">Project Submission Deadline</p>
-        <div
-          className={` flex flex-row w-auto justify-between items-center gap-[5vw] m-5`}
-        >
-          <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal]">
-            <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Days</h1>
-            <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
-              {timer.days > 9 ? timer.days : "0" + timer.days}
-            </p>
+        {total <= 0 ? (
+          <div className="flex flex-col gap-[auto] text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal] py-7 xl:text-7xl md:text-3xl sm:text-sm">Congratulations Hackers!</div>
+        ) : (
+          <div
+            className={` flex flex-row w-auto justify-between items-center gap-[5vw] m-5`}
+          >
+            <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal]">
+              <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Days</h1>
+              <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
+                {timer.days > 9 ? timer.days : "0" + timer.days}
+              </p>
+            </div>
+            <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal]">
+              <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Hours</h1>
+              <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
+                {timer.hours > 9 ? timer.hours : "0" + timer.hours}
+              </p>
+            </div>
+            <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal]">
+              <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Minutes</h1>
+              <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
+                {timer.minutes > 9 ? timer.minutes : "0" + timer.minutes}
+              </p>
+            </div>
+            <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal] xl:block sm:text-sm hidden">
+              <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Seconds</h1>
+              <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
+                {timer.seconds > 9 ? timer.seconds : "0" + timer.seconds}
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal]">
-            <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Hours</h1>
-            <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
-              {timer.hours > 9 ? timer.hours : "0" + timer.hours}
-            </p>
-          </div>
-          <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal]">
-            <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Minutes</h1>
-            <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
-              {timer.minutes > 9 ? timer.minutes : "0" + timer.minutes}
-            </p>
-          </div>
-          <div className="flex flex-col gap-[auto] items-center text-[#FFF5D9] text-center text-xl not-italic font-[575] leading-[normal] xl:block sm:text-sm hidden">
-            <h1 className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">Seconds</h1>
-            <p className="pb-3 xl:text-5xl md:text-3xl sm:text-sm">
-              {timer.seconds > 9 ? timer.seconds : "0" + timer.seconds}
-            </p>
-          </div>
-        </div>
-        <div className="text-center xl:text-5xl md:text-3xl sm:text-sm">
+        )}
+
+        {total <= 0 ? (
+          <></>
+        ) : (
+          <div className="text-center xl:text-5xl md:text-3xl sm:text-sm">
             Submit your project on Devpost
           </div>
+        )}
+
       </div>
 
       <div className=" h-64"></div>
